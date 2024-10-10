65462a9d5c6bfce7202ddb9e371f0f2cfe83708a
Hodroks PC <uspwolf2563@gmail.com>
2024-10-09 19:21:48 -0500

    Deber

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,17 +1,14 @@
 package org.example;
 
 public class Persona {
-
     private String nombre;
     private int edad;
     private String genero;
-
     public Persona(String nombre, int edad, String genero) {
         this.nombre = nombre;
         this.edad = edad;
         this.genero = genero;
     }
-
     public void presentarse() {
         System.out.println("Hola, mi nombre es " + nombre + " y tengo " + edad + " años.");
     }
